8e79ca7a2b58a6a38ec7f6b32354cdd126e2202a
Lhester Castuciano <91195096+TerCstcn@users.noreply.github.com>
2023-03-23 10:35:47 +0800

    [03-23] New Resume Generator

---
+++ Resume/Resume/Controllers/SampleController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Resume.Models;
+
+namespace Resume.Controllers
+{
+    public class SampleController : Controller
+    {
+        private readonly TupContext _tupContext;
+
+        public SampleController(TupContext context )
+        {
+            _tupContext = context;
+        }
+        public IActionResult Index()
+        {
+            var student = _tupContext.Students.OrderByDescending(x => x.Id ).FirstOrDefault();
+                        
+            var vm = new HomeModel
+            {
+                Objectives = HomeController.Objectives,
+                FirstName = student.FirstName,
+                Surname = student.LastName,
+                PlaceBirth = HomeController.PlaceBirth,
+                DateBirth = HomeController.DateBirth,
+                Gender = HomeController.Gender,
+                Address = student.Address,
+                ContactNumber = HomeController.ContactNumber,
+                Email = student.Email,
+                LinkedIn = HomeController.LinkedIn,
+                Website = HomeController.Website,
+                College = HomeController.College,
+                CollegeAdd = HomeController.CollegeAdd,
+                CollegeCourse = HomeController.CollegeCourse,
+                CollegeGrad = HomeController.CollegeGrad,
+                Highschool = HomeController.Highschool,
+                HighschoolAdd = HomeController.HighschoolAdd,
+                HighschoolStrand = HomeController.HighschoolStrand,
+                HighschoolGrad = HomeController.HighschoolGrad,
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ Resume/Resume/Models/EducationalBackground.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Resume.Models;
+
+public partial class EducationalBackground
+{
+    public int Id { get; set; }
+
+    public string? School { get; set; }
+
+    public int? Year { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Course { get; set; }
+
+    public int? StudentId { get; set; }
+
+    public virtual Student? Student { get; set; }
+}
